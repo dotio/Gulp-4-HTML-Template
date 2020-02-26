e74754022f1f5bf9d4c6bb9a12c5f483c819eb76
Dotio <dotio@192.168.1.67>
2020-02-26 19:30:33 +0100

    fix bugs, upd depend, rename

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,13 +1,13 @@
 /* global $ */
 module.exports = () => {
-  $.gulp.task("font:copy", () => {
-    return $.gulp.src("./source/font/**/*.*").pipe($.gulp.dest("./dev/font/"));
+  $.gulp.task('font:copy', () => {
+    return $.gulp.src('./source/fonts/**/*.*').pipe($.gulp.dest('./dev/font/'));
   });
 
-  $.gulp.task("font:build", () => {
+  $.gulp.task('font:build', () => {
     return $.gulp
-      .src("./source/font/**/*.* ")
+      .src('./source/font/**/*.* ')
       .pipe($.gp.fontmin())
-      .pipe($.gulp.dest("./build/font/"));
+      .pipe($.gulp.dest('./build/fonts/'));
   });
 };
